fddbb81e01a11035c64bcd351513229e23f2b4c4
osterdalh <32514828+osterdalh@users.noreply.github.com>
2020-10-27 10:00:16 +0100

    initial navbar

---
+++ src/pages/Dough/Dough.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Dough = () => {
+
+    return(
+        <div className="Dough">
+            <div className="container">
+                <div className="row">
+                    <div className="col-4">
+        Dashboard
+                    </div>
+                    <div className="col-8">
+        settings
+                    </div>
+
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Dough
